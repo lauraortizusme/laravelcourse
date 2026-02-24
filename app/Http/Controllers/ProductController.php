d1a5935656aaeca65d84a27ef4f347ac7aa2009a
Laura <lortizu@eafit.edu.co>
2026-02-24 18:12:47 -0500

    Tutorial Laravel 2

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,53 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class ProductController extends Controller
 {
-    public static $products = [
-        ["id"=>"1", "name"=>"TV", "description"=>"Best TV", "price" => 60],
-        ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone", "price" => 80],
-        ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast", "price" => 100],
-        ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses", "price" => 120]
-    ];
-
     public function index(): View
     {
         $viewData = [];
-        $viewData["title"] = "Products - Online Store";
-        $viewData["subtitle"] =  "List of products";
-        $viewData["products"] = ProductController::$products;
-        return view('product.index')->with("viewData", $viewData);
+        $viewData['title'] = 'Products - Online Store';
+        $viewData['subtitle'] = 'List of products';
+        $viewData['products'] = Product::all();
+
+        return view('product.index')->with('viewData', $viewData);
     }
 
-    public function show(string $id) : View
+    public function show(string $id): View
     {
         $viewData = [];
-        $product = ProductController::$products[$id-1];
-        $viewData["title"] = $product["name"]." - Online Store";
-        $viewData["subtitle"] =  $product["name"]." - Product information";
-        $viewData["product"] = $product;
-        return view('product.show')->with("viewData", $viewData);
+        $product = Product::findOrFail($id);
+        $viewData['title'] = $product['name'].' - Online Store';
+        $viewData['subtitle'] = $product['name'].' - Product information';
+        $viewData['product'] = $product;
+
+        return view('product.show')->with('viewData', $viewData);
     }
 
     public function create(): View
     {
-        $viewData = []; //to be sent to the view
-        $viewData["title"] = "Create product";
+        $viewData = []; // to be sent to the view
+        $viewData['title'] = 'Create product';
 
-        return view('product.create')->with("viewData",$viewData);
+        return view('product.create')->with('viewData', $viewData);
     }
 
-    public function save(Request $request)
+    public function save(Request $request): \Illuminate\Http\RedirectResponse
     {
         $request->validate([
-            "name" => "required",
-            "price" => "required"
+            'name' => 'required',
+            'price' => 'required',
         ]);
-        return view('product.success');
-        //here will be the code to call the model and save it to the database
+
+        Product::create($request->only(['name', 'price']));
+
+        return back();
     }
 }
-
--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -1,41 +1,25 @@
 @extends('layouts.app')
-
 @section('title', $viewData["title"])
 @section('subtitle', $viewData["subtitle"])
-
 @section('content')
-  <div class="card mb-3">
-    <div class="row g-0">
-      <div class="col-md-4">
-        <img
-          src="https://laravel.com/img/logotype.min.svg"
-          class="img-fluid rounded-start"
-        >
-      </div>
+<div class="card mb-3">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://laravel.com/img/logotype.min.svg" class="img-fluid rounded-start">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">
+           {{ $viewData["product"]["name"] }}
+        </h5>
+        <p class="card-text">{{ $viewData["product"]["price"] }}</p>
 
-      <div class="col-md-8">
-        <div class="card-body">
+        @foreach($viewData["product"]->comments as $comment)
+          - {{ $comment->getDescription() }}<br />
+        @endforeach
 
-          @if ($viewData["product"]["price"] > 80)
-            <h5 class="card-title text-danger">
-              {{ $viewData["product"]["name"] }}
-            </h5>
-          @else
-            <h5 class="card-title">
-              {{ $viewData["product"]["name"] }}
-            </h5>
-          @endif
-
-          <p class="card-text">
-            {{ $viewData["product"]["description"] }}
-          </p>
-
-          <p class="card-text">
-            Price: ${{ $viewData["product"]["price"] }}
-          </p>
-
-        </div>
       </div>
     </div>
   </div>
+</div>
 @endsection
